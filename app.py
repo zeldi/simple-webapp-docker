342523c967ba41630d0ed75c7c6b69d33f63de6a
Zeldi Suryady <zeldi.suryady@gmail.com>
2018-05-22 15:16:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
 from flask import Flask
-app = Flask(__name__)
+application = Flask(__name__)
 
 @app.route("/")
 def main():
     return "Welcome!"
 
-@app.route('/how are you')
+@app.route('/howareyou')
 def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run()
+
